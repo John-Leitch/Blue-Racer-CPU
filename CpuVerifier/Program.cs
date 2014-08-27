5ac0d4a139a99a10dc66ba7108efde5254009d60
John-Leitch <johnleitch@outlook.com>
2014-08-26 21:41:51 -0400

    Added CPU Verifier

---
+++ CpuVerifier/Program.cs
@@ -0,0 +1,32 @@
+﻿using Components.BlueRacer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpuVerifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var runner in new[]
+            {
+                new CpuVerificationRunner()
+                {
+                    Verifier = new InstructionStreamVerifier(),
+                    TestPath = @"InstructionSeqTests",
+                },
+                new CpuVerificationRunner()
+                {
+                    Verifier = new CpuSoftwareVerifier(),
+                    TestPath = @"SoftwareTests",
+                }
+            })
+            {
+                runner.Run();
+            }
+        }
+    }
+}
