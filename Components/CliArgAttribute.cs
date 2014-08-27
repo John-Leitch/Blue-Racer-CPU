6f5c7b7011219f656243ef5dfd1ab74c342d2801
John-Leitch <johnleitch@outlook.com>
2014-08-26 20:52:23 -0400

    Added CLI argument classes

---
+++ Components/CliArgAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class CliArgAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public bool IsRequired { get; set; }
+
+        public bool IsInputFile { get; set; }
+
+        public CliArgAttribute(string name)
+        {
+            Name = name;
+            IsRequired = false;
+        }
+    }
+}
